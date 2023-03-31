b61f2202b57a4256d5df75fc7be39eb085e016f6
Da-Scher <Dak.Scher@proton.me>
2023-03-30 19:41:43 -0700

    working stdout reader

--- cmd/categoricalCmd.go
+++ cmd/categoricalCmd.go
@@ -1,12 +1,14 @@
 package cmd
 
 import (
-	"bytes"
 	"fmt"
 	"io"
 	"log"
 	"os"
 	"os/exec"
+	"strings"
+	"syscall"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -19,7 +21,7 @@ var (
 		Short: "Test versions smarter",
 		Long:  "Test versions from latest patch of minor, down.",
 		Run: func(cmd *cobra.Command, args []string) {
-			startCategoryTest()
+			processVersions()
 		},
 	}
 )
@@ -29,18 +31,67 @@ func init() {
 
 }
 
-func startCategoryTest() {
+/*
+	startCategoryTest(version string) -- loop through all the
+
+*
+*/
+func startCategoryTest(version string) {
 	fmt.Println("Categorical test starts here.")
-	testCommand := exec.Command("minepkg", "launch", "vanilla")
-	var stdBuffer bytes.Buffer
-	mw := io.MultiWriter(os.Stdout, &stdBuffer)
+	waitChan := make(chan error)
+
+	testCommand := exec.Command("minepkg", "launch", "vanilla", "--minecraft", version)
+	detector := NewLogDetector()
+	mw := io.MultiWriter(os.Stdout, detector)
 	testCommand.Stdout = mw
 	testCommand.Stderr = mw
-	if err := testCommand.Run(); err != nil {
+
+	if err := testCommand.Start(); err != nil {
 		panic(err)
+	}
+
+	defer testCommand.Process.Signal(syscall.SIGTERM)
+	go func() {
+		waitChan <- testCommand.Wait()
+	}()
+	//fmt.Print("\n\nHELLO\n\n")
+	select {
+	case <-detector.found:
+		fmt.Println("The Duck Digs Dirt")
+		time.Sleep(time.Second)
+		// Stop the process
+	case <-waitChan:
+		break
+	case <-time.After(15 * time.Second):
+		log.Printf("%s :: Minecraft did not exit, stopping '''peacefully'''\n", version)
 	}
 
 	fmt.Println("> minepkgStressTest")
 
-	log.Println(stdBuffer.String())
+}
+
+type prog struct {
+}
+
+func processVersions() {
+	url := "https://launchermeta.mojang.com/mc/game/version_manifest.json"
+
+}
+
+type logDetector struct {
+	found chan bool
+}
+
+func (l *logDetector) Write(p []byte) (n int, err error) {
+	if strings.Contains(string(p), "[Render thread/INFO]: Created: 128x128x0 minecraft:textures/atlas/bom_effects.png-atlas") {
+		log.Println("[StressTest]: Important string found!")
+		l.found <- true
+	}
+	return len(p), nil
+}
+
+func NewLogDetector() *logDetector {
+	return &logDetector{
+		found: make(chan bool),
+	}
 }
